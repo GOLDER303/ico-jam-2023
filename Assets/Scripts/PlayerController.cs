3b0ee2aeecf94ffa53b2ea7cde1a98c803e3359a
GOLDER303 <szymonstepien@interia.pl>
2023-07-30 13:34:31 +0200

    Add player shape-shifting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,27 @@ public class PlayerController : MonoBehaviour
     [SerializeField] int maxPositionsToSide = 1;
     [SerializeField] float moveSpeed = 15f;
     [SerializeField] float positionDistance = 5f;
+    [SerializeField] PlayerShapeSO[] availablePlayerShapeSOs;
+
+    public PlayerShapeSO currentPlayerShapeSO { private set; get; }
 
     private Rigidbody playerRigidBody;
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
     private Vector3 targetPlayerPosition;
 
     private void Awake()
     {
         playerRigidBody = GetComponent<Rigidbody>();
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     private void Start()
     {
         targetPlayerPosition = transform.position;
+
+        StartCoroutine(ShapeChangeCoroutine());
     }
 
     private void Update()
@@ -36,9 +45,21 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
-
-
-
         targetPlayerPosition.x = targetPlayerPosition.x + (inputVector.x * positionDistance);
+    }
+
+    private IEnumerator ShapeChangeCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+
+            PlayerShapeSO nextPlayerShapeSO = availablePlayerShapeSOs[Random.Range(0, availablePlayerShapeSOs.Length)];
+
+            meshFilter.mesh = nextPlayerShapeSO.mesh;
+            meshRenderer.material = nextPlayerShapeSO.material;
+
+            currentPlayerShapeSO = nextPlayerShapeSO;
+        }
     }
 }
---
+++ Assets/Scripts/PlayerShapeSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class PlayerShapeSO : ScriptableObject
+{
+    public Mesh mesh;
+    public Material material;
+}
